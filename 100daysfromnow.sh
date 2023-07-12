fcec3ac98849317b828f2295ed4fa6e4f8c62d34
kushkota <57915988+kushkota@users.noreply.github.com>
2023-07-12 01:46:35 +0000

    Tested on macOS

--- 100daysfromnow.sh
+++ 100daysfromnow.sh
@@ -3,6 +3,10 @@
 # Create a program that tells us
 # what day it is 100 days from today
 
+# Tested on Linux and macOS operating system
 echo -n "100 days from now is... "
- 
-date -d "100 days" +'%Y-%m-%d'
+if [ "$(uname -s)" == "Linux" ]; then
+date -u -d "100 days" +'%Y-%m-%d'
+else
+date -u -v+100d +%Y-%m-%d
+fi
